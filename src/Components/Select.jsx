67a06828ee82cfd457a6889ebdedcb38959e96da
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-03-28 00:57:16 +0530

    Select section bug fixed

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -14,7 +14,7 @@ function Select({
             </label>}
             <select {...props} id={id} ref={ref}  className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-100 duration-200 border border-gray-200 w-full ${className}`}>
                 {options?.map((option)=>(
-                    <option  value={option} key={option}>
+                    <option key={option}>
                         {option}
                     </option>
                 ))}
